b2b06d22d42e9516670078203a3dec68992bfa9a
alpaunovski <aleksandar@paunovsky.com>
2021-05-01 09:08:07 +0300

    _

--- app.js
+++ app.js
@@ -9,14 +9,28 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+const path = require('path');
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+app.get("/", function(req, res) {
+    res.render("home", { startingContent: homeStartingContent });
 
 
+});
+
+app.get("/about", function(req, res) {
+    res.render("about", { aboutText: aboutContent })
+});
+
+app.get("/contact", function(req, res) {
+    res.render("contact", { contactText: contactContent })
+});
 
 
 
@@ -29,5 +43,5 @@ app.use(express.static("public"));
 
 
 app.listen(3000, function() {
-  console.log("Server started on port 3000");
+    console.log("Server started on port 3000");
 });
